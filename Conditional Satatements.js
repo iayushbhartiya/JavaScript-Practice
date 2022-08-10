9fa9969d90a7eafca638a150c506faa36feaf8f8
Ayush Gupta <iamayush18@outlook.com>
2022-08-11 03:10:42 +0530

    Conditional_statements

---
+++ Conditional Satatements.js
@@ -0,0 +1,16 @@
+let num1 = 3; num2 = 4; num3 = 1;
+
+if(num1 < num2 && num1<num3){
+    console.log('num2 and num3 are greater than num1');
+}
+
+else if(num1 < num2 && num1 > num3){
+    console.log('num1 is less than num2 but greater than num3')
+}
+
+else{
+    console.log('num1 is greater');
+    
+}
+
+console.log('Bye...');
---
+++ temp1.js
@@ -0,0 +1,19 @@
+let num = 10;
+
+if(num % 2 === 0){
+    console.log("number is even");
+}
+
+else{
+    console.log("number is odd");
+}
+
+// the above can also bet written as 
+
+let res
+
+res = num % 2 === 0 ? "Even" : "Odd"
+
+console.log(res);
+
+// above is ternary operator ? :
